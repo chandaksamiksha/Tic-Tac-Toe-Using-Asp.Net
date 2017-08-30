45c3733ee6276c459b48342b6dff0e15c464381c
Samiksha Chandak <schandak@tavisca.com>
2017-08-30 17:56:12 +0530

    basic implementation of tic tac toe using asp.net

---
+++ TicTacToe/Default.aspx
@@ -0,0 +1,70 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="TicTacToe.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Tic Tac Toe Game</title>
+    <link rel="stylesheet" type="text/css" />
+    <style type="text/css">
+        #winnerMessage {
+            width: 77px;
+        }
+        .auto-style1 {
+            width: 45px;
+        }
+        .auto-style2 {
+            width: 194px;
+            height: 40px;
+        }
+    </style>
+</head>
+<body>
+<form id="index" runat="server">
+<header>
+<h3 style="width: 152px">Tic Tac Toe</h3>
+</header>
+<section>
+<article id="mainGame">
+ <table id="board">
+<tr>
+<td><asp:Button ID="Button1" runat="server" Height="45px" Width="45px" OnClick="Button1_Click" Text="." /></td>
+ <td><asp:Button ID="Button2" runat="server" Height="45px" Width="45px" OnClick="Button2_Click" Text="." /></td>
+<td class="auto-style1"><asp:Button ID="Button3" runat="server" Height="45px" Width="45px" OnClick="Button3_Click" Text="." /></td>
+</tr>
+<tr>
+<td><asp:Button ID="Button4" runat="server" Height="45px" Width="45px" OnClick="Button4_Click" Text="." /> </td>
+<td><asp:Button ID="Button5" runat="server" Style="width: 45px; height: 45px;" OnClick="Button5_Click" Text="." /></td>
+<td class="auto-style1"><asp:Button ID="Button6" runat="server" Style="width: 45px; height: 45px;" OnClick="Button6_Click" Text="." /></td>
+</tr>
+<tr>
+<td><asp:Button ID="Button7" runat="server" Height="45px" Width="45px" OnClick="Button7_Click" Text="." /></td>
+<td><asp:Button ID="Button8" runat="server"  Style="width: 45px; height: 45px;" OnClick="Button8_Click" Text="." /></td>
+<td class="auto-style1"><asp:Button ID="Button9" runat="server"  Style="width: 45px; height: 45px;" OnClick="Button9_Click" Text="." /></td>
+</tr>
+</table>
+</article>
+
+<article id="TurnLabel" runat="server">
+    <br />
+    <asp:Label ID="Label2" runat="server" Text="Turn"></asp:Label>
+    <br />
+</article>
+
+<article id="currentTurn" runat="server">
+<div class="auto-style2">
+    <asp:Label ID="Label1" runat="server"></asp:Label>
+    <br />
+    <br />
+    </div>
+</article>
+</section>
+<br />
+<br />
+
+    <asp:Button ID="Reset" runat="server" Text="Reset" Width="139px" OnClick="Reset_Click" />
+&nbsp;
+    <asp:Button ID="Button12" runat="server" Text="Play Again" Width="139px" OnClick="Button12_Click" />
+</form>
+</body>
+</html>
---
+++ TicTacToe/Default.aspx.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TicTacToe
+{
+    public partial class Default : System.Web.UI.Page
+    {
+       static int player = 1;
+        int hasWinner = 0;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (player == 1 && Button1.Text.Equals("."))
+            {
+                Button1.Text = "X";
+                player = 2;
+                Label1.Text = "Player 2 ";
+                WinningCondition(Button1.Text);
+            }
+            if (player == 2 && Button1.Text.Equals("."))
+            {
+                Button1.Text = "O";
+                player = 1;
+                Label1.Text = "Player 1 ";
+                WinningCondition(Button1.Text);
+            }
+            DrawCondition();
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            if (player == 1 && Button2.Text.Equals("."))
+            {
+                Button2.Text = "X";
+                player = 2;
+                Label1.Text = "Player 2 ";
+                WinningCondition(Button2.Text);
+            }
+            if (player == 2 && Button2.Text.Equals("."))
+            {
+                Button2.Text = "O";
+                player = 1;
+                Label1.Text = "Player 1 ";
+                WinningCondition(Button2.Text);
+            }
+            DrawCondition();
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            if (player == 1 && Button3.Text.Equals("."))
+            {
+                Button3.Text = "X";
+                player = 2;
+                Label1.Text = "Player 2 ";
+                WinningCondition(Button3.Text);
+            }
+            if (player == 2 && Button3.Text.Equals("."))
+            {
+                Button3.Text = "O";
+                player = 1;
+                Label1.Text = "Player 1 ";
+                WinningCondition(Button3.Text);
+            }
+            DrawCondition();
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            if (player == 1 && Button4.Text.Equals("."))
+            {
+                Button4.Text = "X";
+                player = 2;
+                Label1.Text = "Player 2 ";
+                WinningCondition(Button4.Text);
+            }
+            if (player == 2 && Button4.Text.Equals("."))
+            {
+                Button4.Text = "O";
+                player = 1;
+                Label1.Text = "Player 1 ";
+                WinningCondition(Button4.Text);
+            }
+            DrawCondition();
+        }
+
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            if (player == 1 && Button5.Text.Equals("."))
+            {
+                Button5.Text = "X";
+                player = 2;
+                Label1.Text = "Player 2 ";
+                WinningCondition(Button5.Text);
+            }
+            if (player == 2 && Button5.Text.Equals("."))
+            {
+                Button5.Text = "O";
+                player = 1;
+                Label1.Text = "Player 1 ";
+                WinningCondition(Button5.Text);
+            }
+            DrawCondition();
+        }
+
+        protected void Button6_Click(object sender, EventArgs e)
+        {
+            if (player == 1 && Button6.Text.Equals("."))
+            {
+                Button6.Text = "X";
+                player = 2;
+                Label1.Text = "Player 2 ";
+                WinningCondition(Button6.Text);
+            }
+            if (player == 2 && Button6.Text.Equals("."))
+            {
+                Button6.Text = "O";
+                player = 1;
+                Label1.Text = "Player 1 ";
+                WinningCondition(Button6.Text);
+            }
+            DrawCondition();
+        }
+
+        protected void Button7_Click(object sender, EventArgs e)
+        {
+            if (player == 1 && Button7.Text.Equals("."))
+            {
+                Button7.Text = "X";
+                player = 2;
+                Label1.Text = "Player 2 ";
+                WinningCondition(Button7.Text);
+            }
+            if (player == 2 && Button7.Text.Equals("."))
+            {
+                Button7.Text = "O";
+                player = 1;
+                Label1.Text = "Player 1 ";
+                WinningCondition(Button7.Text);
+            }
+            DrawCondition();
+        }
+
+        protected void Button8_Click(object sender, EventArgs e)
+        {
+            if (player == 1 && Button8.Text.Equals("."))
+            {
+                Button8.Text = "X";
+                player = 2;
+                Label1.Text = "Player 2 ";
+                WinningCondition(Button8.Text);
+            }
+            if (player == 2 && Button8.Text.Equals("."))
+            {
+                Button8.Text = "O";
+                player = 1;
+                Label1.Text = "Player 1 ";
+                WinningCondition(Button8.Text);
+            }
+            DrawCondition();
+        }
+
+        protected void Button9_Click(object sender, EventArgs e)
+        {
+            if (player == 1 && Button9.Text.Equals("."))
+            {
+                Button9.Text = "X";
+                player = 2;
+                Label1.Text = "Player 2 ";
+                WinningCondition(Button9.Text);
+            }
+            if (player == 2 && Button9.Text.Equals("."))
+            {
+                Button9.Text = "O";
+                player = 1;
+                Label1.Text = "Player 1 ";
+                WinningCondition(Button9.Text);
+            }
+            DrawCondition();
+        }
+
+        protected void WinningCondition(string text)
+        {
+            if ((Button1.Text.Equals(text) && Button2.Text.Equals(text) && Button3.Text.Equals(text)) ||
+                     (Button4.Text.Equals(text) && Button5.Text.Equals(text) && Button6.Text.Equals(text)) ||
+                     (Button7.Text.Equals(text) && Button8.Text.Equals(text) && Button9.Text.Equals(text)) ||
+                     (Button1.Text.Equals(text) && Button4.Text.Equals(text) && Button7.Text.Equals(text)) ||
+                     (Button2.Text.Equals(text) && Button5.Text.Equals(text) && Button8.Text.Equals(text)) ||
+                     (Button3.Text.Equals(text) && Button6.Text.Equals(text) && Button9.Text.Equals(text)) ||
+                     (Button1.Text.Equals(text) && Button5.Text.Equals(text) && Button9.Text.Equals(text)) ||
+                     (Button3.Text.Equals(text) && Button5.Text.Equals(text) && Button7.Text.Equals(text)))
+            {
+                if (player == 1)
+                {
+                    Label2.Text = "Match's Status";
+                    Label1.Text = "Player 2 wins";
+                    hasWinner = 1;
+                    ResetGame();
+                }
+                else
+                {
+                    Label2.Text = "Match's Status";
+                    Label1.Text = "Player1 wins";
+                    hasWinner = 1;
+                    ResetGame();
+                }
+            }
+        }
+
+        protected void DrawCondition()
+        {
+            if(Button1.Text!="." && Button2.Text != "." && Button3.Text != "." && Button4.Text != "." &&
+                Button5.Text != "." && Button6.Text != "." && Button7.Text != "." && Button8.Text != "." &&
+                Button9.Text != "." && hasWinner==0 )
+            {
+                Label2.Text = "Match's Status";
+                Label1.Text = "Match Draw";
+            }
+        }
+
+        protected void Reset_Click(object sender, EventArgs e)
+        {
+            ResetGame();
+        }
+
+        protected void ResetGame()
+        {
+            Button1.Text = ".";
+            Button2.Text = ".";
+            Button3.Text = ".";
+            Button4.Text = ".";
+            Button5.Text = ".";
+            Button6.Text = ".";
+            Button7.Text = ".";
+            Button8.Text = ".";
+            Button9.Text = ".";
+        }
+
+        protected void Button12_Click(object sender, EventArgs e)
+        {
+            ResetGame();
+        }
+    }
+}
